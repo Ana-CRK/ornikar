73bc3d8f07312f2fc8f361db0e50ed6d545b6a7e
Anastasiia <anastassiia.k@gmail.com>
2022-04-10 17:27:13 +0200

    Refactor TemplateManager (add replaceIfContains)

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -29,52 +29,43 @@ class TemplateManager
     {
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
 
-        $lesson = (isset($data['lesson']) and $data['lesson'] instanceof Lesson) ? $data['lesson'] : null;
+        if (!$data['lesson'] instanceof Lesson) return null;
 
-        if ($lesson)
-        {
-            $_lessonFromRepository = LessonRepository::getInstance()->getById($lesson->id);
-            $usefulObject = MeetingPointRepository::getInstance()->getById($lesson->meetingPointId);
-            $instructorOfLesson = InstructorRepository::getInstance()->getById($lesson->instructorId);
+        $lesson = $data['lesson'];
 
-            if($this->str_contains($text, '[lesson:instructor_link]')){
-                $text = str_replace('[instructor_link]',  'instructors/' . $instructorOfLesson->id .'-'.urlencode($instructorOfLesson->firstname), $text);
-            }
+        $_lessonFromRepository = LessonRepository::getInstance()->getById($lesson->id);
 
-            $containsSummaryHtml = strpos($text, '[lesson:summary_html]');
-            $containsSummary     = strpos($text, '[lesson:summary]');
+        $usefulObject = MeetingPointRepository::getInstance()->getById($lesson->meetingPointId);
+        $instructorOfLesson = InstructorRepository::getInstance()->getById($lesson->instructorId);
 
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[lesson:summary_html]',
-                        Lesson::renderHtml($_lessonFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[lesson:summary]',
-                        Lesson::renderText($_lessonFromRepository),
-                        $text
-                    );}}
-
-            ($this->str_contains($text, '[lesson:instructor_name]')) and $text = str_replace('[lesson:instructor_name]',$instructorOfLesson->firstname,$text);
+        if($this->str_contains($text, '[lesson:instructor_link]')){
+            $text = str_replace('[instructor_link]',  'instructors/' . $instructorOfLesson->id .'-'.urlencode($instructorOfLesson->firstname), $text);
         }
 
-        if ($lesson->meetingPointId) {
-            if($this->str_contains($text, '[lesson:meeting_point]'))
-                $text = str_replace('[lesson:meeting_point]', $usefulObject->name, $text);
+        if ($this->str_contains($text, '[lesson:summary_html]')) {
+            $text = str_replace(
+                '[lesson:summary_html]',
+                Lesson::renderHtml($_lessonFromRepository),
+                $text
+            );
+        }
+        if ($this->str_contains($text, '[lesson:summary]')) {
+            $text = str_replace(
+                '[lesson:summary]',
+                Lesson::renderText($_lessonFromRepository),
+                $text
+            );
         }
 
-        if($this->str_contains($text, '[lesson:start_date]'))
-            $text = str_replace('[lesson:start_date]', $lesson->start_time->format('d/m/Y'), $text);
+        $text = $this->replaceIfContains($text, '[lesson:instructor_name]', $instructorOfLesson->firstname);
 
-        if($this->str_contains($text, '[lesson:start_time]'))
-            $text = str_replace('[lesson:start_time]', $lesson->start_time->format('H:i'), $text);
+        $text = $this->replaceIfContains($text, '[lesson:meeting_point]', $usefulObject->name);
 
-        if($this->str_contains($text, '[lesson:end_time]'))
-            $text = str_replace('[lesson:end_time]', $lesson->end_time->format('H:i'), $text);
+        $text = $this->replaceIfContains($text, '[lesson:start_date]', $lesson->start_time->format('d/m/Y'));
+
+        $text = $this->replaceIfContains($text, '[lesson:start_time]', $lesson->start_time->format('H:i'));
+
+        $text = $this->replaceIfContains($text, '[lesson:end_time]', $lesson->end_time->format('H:i'), $text);
 
 
             if (isset($data['instructor'])  and ($data['instructor']  instanceof Instructor))
@@ -86,13 +77,22 @@ class TemplateManager
          * USER
          * [user:*]
          */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof Learner))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
+        $_user = (isset($data['user']) and ($data['user'] instanceof Learner)) ? $data['user'] : $APPLICATION_CONTEXT->getCurrentUser();
         if($_user) {
-            ($this->str_contains($text, '[user:first_name]')) and $text = str_replace('[user:first_name]'       , ucfirst(strtolower($_user->firstname)), $text);
+            ($this->str_contains($text, '[user:first_name]')) and $text = str_replace('[user:first_name]', ucfirst(strtolower($_user->firstname)), $text);
         }
 
         return $text;
     }
+
+    private function replaceIfContains(string $text, string $needle, string $replace): string 
+    {
+        if ($this->str_contains($text, $needle)) {
+            $text = str_replace($needle, $replace, $text);
+        }
+
+        return $text;
+    } 
 
     /* 
      * PHP 8 str_contains — Determine if a string contains a given substring
